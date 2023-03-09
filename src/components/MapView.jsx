cbfa121e7f1d192481f4e4da6a2fac0825c35ee0
Gonzalo Morales <114691980+Gonzalomorales1001@users.noreply.github.com>
2023-03-09 18:06:00 -0300

    Update MapView.jsx

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -34,7 +34,7 @@ const MapView = ({ipInfo}) => {
   return (
     <>
     <section>
-    <MapContainer ref={mapRef} center={[0,0]} zoom={1} scrollWheelZoom={true}>
+    <MapContainer ref={mapRef} center={[0,0]} zoom={2} scrollWheelZoom={true}>
       <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
