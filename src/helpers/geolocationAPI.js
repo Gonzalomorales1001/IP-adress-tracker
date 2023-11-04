0df39ac37f9bfd8eda670feaa95c987b9b3432cc
Gonzalo Morales <gonzalomorales1001@gmail.com>
2023-11-04 14:24:47 -0300

    UPDATE: Hidden API_KEY

--- src/helpers/geolocationAPI.js
+++ src/helpers/geolocationAPI.js
@@ -1,11 +1,13 @@
-export const requestGeoAPI=async(ip)=>{
-    let response= await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_2uZUE7YTcLk6KAuWdXfRhf4EXpSQM&ipAddress=${ip}`)
-    let data= response.json()
+import { API_KEY } from './API_KEY'
+
+export const requestGeoAPI = async (ip) => {
+    let response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`)
+    let data = response.json()
     return data
 }
 
-export const getMyIP=async()=>{
-    let response= await fetch (`https://api.ipify.org?format=json`)
-    let data=response.json()
+export const getMyIP = async () => {
+    let response = await fetch(`https://api.ipify.org?format=json`)
+    let data = response.json()
     return data
 }
